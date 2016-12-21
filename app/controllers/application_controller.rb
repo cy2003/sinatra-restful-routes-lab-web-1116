507f2efa8c896efc6328cc6db5a01a5a5218e94e
cy2003 <catyyee@hotmail.com>
2016-12-20 22:19:02 -0500

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,52 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    #extracting the form data and putting it in code and using it to create an instance of post class
+    @recipe = Recipe.create(name: params["name"], ingredients: params["ingredients"], cook_time: params["cook_time"])
+    # show.erb shows the post that were created
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
   end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe= Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params[:recipe])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe= Recipe.find(params[:id])
+    @name = @recipe.name
+    @recipe.destroy
+    erb :delete
+  end
+
+  # post '/recipes' do
+  #
+  # end
+
 end
